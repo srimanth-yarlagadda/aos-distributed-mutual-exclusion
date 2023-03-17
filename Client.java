1f2c1580cbe56e506151bd8b3047254ae5fe2a0d
srimanth-yarlagadda <srimanth.y@gmail.com>
2023-03-16 21:36:14 -0500

    basic distributed mutex - client server comms w CS

--- Client.java
+++ Client.java
@@ -2,8 +2,11 @@ import java.net.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.util.*;
+import java.sql.*;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
+    private static boolean systemDebug = false;
     private Socket serverConnection;
     private static int Request = 1;
     private static int Release = 2;
@@ -11,13 +14,57 @@ public class Client {
     private static int Grant = 4;
     private static int Wait = 5;
 
+    private static List<List<Integer>> quorumSet = new ArrayList<List<Integer>>();
+    private static Set<Integer> grantList = new HashSet<Integer>();
+    private static boolean CriticalSectionCompletion = false;
+
     // Establish connection with Server & create input & output stream objects
     public void lockServer(String serverIP, int port) {
         System.out.println("Connection request: " + serverIP + port);
         try {
             serverConnection = new Socket(serverIP, port);
-            System.out.println("Connection Successful: " + serverConnection);
-        } catch (IOException except) {
+            System.out.println("Connection Successful: " + serverConnection.getInetAddress().getHostName().toString());
+            // TimeUnit.SECONDS.sleep(3);
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+
+
+            DataOutputStream outChannel = new DataOutputStream(serverConnection.getOutputStream());
+            DataInputStream inputDataStream = new DataInputStream(serverConnection.getInputStream());
+            
+            Request makeRequest = new Request();
+            makeRequest.clientID = Integer.parseInt(InetAddress.getLocalHost().toString().split("\\.")[0].substring(2,4));
+            makeRequest.requestTimestamp = new Timestamp(System.currentTimeMillis());
+
+            objectOutputStream.writeObject(makeRequest);
+            byte[] byteData = byteArrayOutputStream.toByteArray();
+        
+            outChannel.write(byteData);
+            System.out.println("Sending a request: " + makeRequest.clientID + "@" + makeRequest.requestTimestamp);
+
+            while (true) {
+                int getResponse = inputDataStream.readInt();
+                if (getResponse == Grant) {
+                    break;
+                }
+            }
+
+            grantList.add(Integer.parseInt(serverConnection.getInetAddress().getHostName().toString().split("\\.")[0].substring(2,4)));
+
+            while (!CriticalSectionCompletion) {
+                try {
+                    TimeUnit.MICROSECONDS.sleep(5);
+                } catch (InterruptedException exc) {
+                    exc.printStackTrace();
+                }
+            }
+            
+            System.out.println("Releasing lock ... ");
+            outChannel.writeInt(Release);
+            System.out.println("Released Lock !");
+
+        } catch (IOException /*| InterruptedException*/ except) {
             System.err.println("Connection failed!");
             except.printStackTrace();
         }
@@ -25,7 +72,7 @@ public class Client {
 
     public List<String> generateQuorumIPAddresses(int serverID) {
         List<String> serverList = new ArrayList<String>();
-        if (serverID >= 4) {
+        if (serverID < 4) {
             serverList.add(String.format("dc%02d.utdallas.edu:%d", serverID, 9037 + serverID));
         } else {
             serverList.add(String.format("dc%02d.utdallas.edu:%d", serverID, 9037 + serverID));
@@ -42,7 +89,7 @@ public class Client {
         for (int i = 0; i < serverList.size(); i++) {
             // System.out.println("Begin Lock attempt: " + i);
             final String serverAddress = serverList.get(i);
-            System.out.println("Begin Lock attempt: " + serverAddress + serverList.get(i));
+            System.out.println("Begin Lock attempt: " + serverAddress);
             Thread lockServerThread = new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -56,14 +103,57 @@ public class Client {
         System.out.println(requiredGrant);
     }
 
+    public void criticalSection() {
+        boolean criticalSectionGrant = false;
+        while (!criticalSectionGrant) {
+            for (int i = 0; i < quorumSet.size(); i++) {
+                List<Integer> quorum = quorumSet.get(i);
+                int required = quorum.size();
+                for (int j = 0; j < quorum.size(); j++) {
+                    int servID = quorum.get(j);
+                    if (grantList.contains(servID)) {
+                        if (systemDebug) {System.out.println("\n\n\nFound in grants " + servID);}
+                        required--;
+                    }
+                }
+                if (required <= 0) {
+                    criticalSectionGrant = true;
+                    break;
+                }
+            }
+        }
+        System.out.println("Entering critical section...");
+        try {
+            TimeUnit.SECONDS.sleep(6);
+            CriticalSectionCompletion = true;
+        } catch (InterruptedException exc) {
+            exc.printStackTrace();
+        }
+        System.out.println("Exiting critical section !");
+    }
+
     public static void main(String[] args) {
         /* IP address and Port declarations. Start connection */
         String ipaddress = "dc01.utdallas.edu";
         int port = 9038;
         // Client client =new Client();
 
+        List<Integer> quorum1 = new ArrayList<Integer>();
+        quorum1.add(1);
+        // quorum1.add(2);
+        // quorum1.add(3);
+        quorumSet.add(quorum1);
+
+        quorum1 = new ArrayList<Integer>();
+        quorum1.add(1);
+        quorum1.add(2);
+        quorum1.add(4);
+        quorumSet.add(quorum1);
+
+        System.out.println("Quorum set:" + quorumSet);
+
         Random randomServerIDGenerator = new Random();
-        final int mutexGrantServer = 1 + randomServerIDGenerator.nextInt(8);
+        final int mutexGrantServer = 1; //1 + randomServerIDGenerator.nextInt(8); //
         Thread getGrantThread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -72,9 +162,23 @@ public class Client {
         });
         getGrantThread.start();
 
+        Thread criticalSectionThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                new Client().criticalSection();
+            }
+        });
+        criticalSectionThread.start();
+
         // client.startConnection(ipaddress, port);
 
-        System.out.println("\n=== END ===\n");
+        // System.out.println("\n=== END ===\n");
 
     }
+}
+
+class Request implements Serializable {
+    int clientID;
+    Timestamp requestTimestamp;
+    boolean Grant;
 }
--- Server.java
+++ Server.java
@@ -5,8 +5,13 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.*;
 import java.nio.file.Files;
 import java.net.InetAddress;
+import java.sql.*;
+import java.util.concurrent.TimeUnit;
+
 
 public class Server implements Runnable {
+
+    private static boolean systemDebug = false;
     
     private static Semaphore tokenSemaphore;
     private static ServerSocket serverSocket;
@@ -16,11 +21,15 @@ public class Server implements Runnable {
     private static int debug = 0;
     private static Integer myPort;
 
+    public static boolean killMain = false;
+
     private static int Request = 1;
     private static int Release = 2;
     private static int Relinquish = 3;
     private static int Grant = 4;
     private static int Wait = 5;
+
+    private static Deque<Request> requestQueue = new ArrayDeque<>();
     
     // Create ServerSocket on the given port
     public void startServer(int port) {
@@ -53,6 +62,7 @@ public class Server implements Runnable {
             System.err.println("Server creation failed !");
             e.printStackTrace();
         }
+        System.out.println("\n\n\nEnding Server");
     }
 
     public void connectTreeParent() {
@@ -82,17 +92,97 @@ public class Server implements Runnable {
 
     public void manageClients(Socket clientSocket) {
         long currentThread = Thread.currentThread().getId();
-        System.out.println("Thread: " + currentThread + " handling\033[1m\033[32m client: \033[0m " + clientSocket);
+        System.out.println("Thread: " + currentThread + " handling\033[1m\033[32m client:\033[0m " + clientSocket.getInetAddress().getHostName().toString());
+        try {
+
+            DataInputStream inputDataStream = new DataInputStream(clientSocket.getInputStream());
+            byte[] getRequestBytes = new byte[1024];
+            int reqSize = inputDataStream.read(getRequestBytes);
+            byte[] getRequestBytesFinal = new byte[reqSize];
+            System.arraycopy(getRequestBytes, 0, getRequestBytesFinal, 0, reqSize);
+
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(getRequestBytesFinal);
+            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+            Request clientRequest = (Request) objectInputStream.readObject();
+            clientRequest.Grant = false;
+            requestQueue.addLast(clientRequest);
+            System.out.println("Received a request: " + clientRequest.clientID + " @ " + clientRequest.requestTimestamp + " => " + clientRequest.Grant);
+
+            while (clientRequest.Grant == false) {
+                if (systemDebug) {System.out.println("Status: " + clientRequest.Grant);}
+                try {
+                    TimeUnit.MICROSECONDS.sleep(6);
+                } catch (InterruptedException exc) {
+                    exc.printStackTrace();
+                }
+            }
+
+            // System.out.println(String.format("=> Granting ... %2d !", clientRequest.clientID));
+            // try {
+            //     TimeUnit.SECONDS.sleep(5);
+            // } catch (InterruptedException exc) {
+            //     exc.printStackTrace();
+            // }
+            System.out.println(String.format("=> Request %2d Granted !", clientRequest.clientID));
+
+            DataOutputStream outputDataStream = new DataOutputStream(clientSocket.getOutputStream());
+            outputDataStream.writeInt(Grant);
+            while (true) {
+                int getRelease = inputDataStream.readInt();
+                if (getRelease == Release) {
+                    break;
+                }
+            }
+
+            tokenSemaphore.release();
+            System.out.println("Thread: " + currentThread + " \033[1m\033[32mcomplete\033[0m, client: " + clientSocket.getInetAddress().getHostName().toString());
+
+        } catch (IOException | ClassNotFoundException except) {
+            except.printStackTrace();
+        }
     }
 
     public void manageChild(Socket clientSocket) {
         long currentThread = Thread.currentThread().getId();
-        System.out.println("Thread: " + currentThread + " handling\033[1m\033[34m child: \033[0m" + clientSocket);
+        System.out.println("Thread: " + currentThread + " handling\033[1m\033[34m child:\033[0m " + clientSocket.getInetAddress().getHostName().toString());
+    }
+
+    public void grantRequests() {
+        while (true) {
+            // Do nothing
+        }
     }
 
     public void run() {
         long currentThread = Thread.currentThread().getId();
         System.out.println("Thread: " + currentThread + " in run() method !");
+    }
+
+    public void grantOperations() {
+        while (!killMain) {
+            if (systemDebug) {System.out.println("Grant ops" + requestQueue.size());}
+            if (requestQueue.size() > 0) {
+                try {
+                    tokenSemaphore.acquire();
+                } catch (InterruptedException exc) {
+                    exc.printStackTrace();
+                }
+                Request currentGrant = requestQueue.removeFirst();
+                currentGrant.Grant = true;
+                System.out.println("Granted request: " + currentGrant.clientID);
+                try {
+                    TimeUnit.SECONDS.sleep(6);
+                } catch (InterruptedException exc) {
+                    exc.printStackTrace();
+                }
+            } else {
+                try {
+                    TimeUnit.MICROSECONDS.sleep(5);
+                } catch (InterruptedException exc) {
+                    exc.printStackTrace();
+                }
+            }
+        } 
     }
 
     public static void main(String[] args) {
@@ -105,18 +195,17 @@ public class Server implements Runnable {
             System.err.println("Host Unknown");
             except.printStackTrace();
         }
+
         tokenSemaphore = new Semaphore(1);
-
-        // Server server=new Server();
-
+        
         Runnable createServer = new Runnable() {
             public void run() {
                 try {
                     serverSocket = new ServerSocket(myPort);
                     System.out.println("Server started:\n" + serverSocket);         
-                } catch (IOException e) {
+                } catch (IOException except) {
                     System.err.println("Server creation failed !");
-                    e.printStackTrace();
+                    except.printStackTrace();
                 }
             }
         };
@@ -131,6 +220,14 @@ public class Server implements Runnable {
         });
         serverThread.start();
 
+        Thread grantRequestsThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                new Server().grantRequests();
+            }
+        });
+        grantRequestsThread.start();
+
         Thread connectParentThread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -138,7 +235,41 @@ public class Server implements Runnable {
             }
         });
         connectParentThread.start();
+
+        Thread grantOperationsThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                new Server().grantOperations();
+            }
+        });
+        grantOperationsThread.start();
+        
         // System.out.println("\n=== SERVER END ===\n");
+        // Deque<Integer> myDeque = new ArrayDeque<>();
+        // myDeque.addLast(1);
+        // myDeque.addLast(2);
+        // System.out.println("\n\n\n" + myDeque.size());
+        // int val = myDeque.removeFirst();
+        // System.out.println(val);
+        // myDeque.addFirst(3);
+        // // System.out.println(myDeque);
+        // try {
+        //     while (true) {
+        //         TimeUnit.SECONDS.sleep(20);
+        //         System.out.println("Main Thread wait..");
+        //     }
+        // } catch (InterruptedException except) {
+        //     except.printStackTrace();
+        // }
+
+
     }
+}
+
+
+class Request implements Serializable {
+    int clientID;
+    Timestamp requestTimestamp;
+    boolean Grant = false;
 }
 
